cd96a15c634c0e91c2f5238f6ce7d9139f4ae0fa
ankit <code7lancer@email.com>
2023-12-14 17:46:21 -0800

    initial commit

---
+++ app/src/main/java/com/x64technology/linex/database/noroom/DBStrings.java
@@ -0,0 +1,11 @@
+package com.x64technology.linex.database.noroom;
+
+public class DBStrings {
+    public static final String ID = "id";
+    public static final String SENDER_ID = "sender_id";
+    public static final String SENDER_USERNAME = "sender_username";
+    public static final String CONTENT = "content";
+    public static final String TIME = "time";
+
+
+}
