b1986c63320b1e50621e852d687da1416004fcdd
ankit <code7lancer@email.com>
2023-12-17 08:45:50 -0800

    connected to socket server

--- app/src/main/java/com/x64technology/linex/MainActivity.java
+++ app/src/main/java/com/x64technology/linex/MainActivity.java
@@ -2,25 +2,29 @@ package com.x64technology.linex;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import com.google.android.material.navigation.NavigationView;
 import com.x64technology.linex.adapters.ChatsAdapter;
 import com.x64technology.linex.database.chat.ChatViewModel;
 import com.x64technology.linex.databinding.ActivityMainBinding;
+import com.x64technology.linex.screens.Auth;
 import com.x64technology.linex.screens.ContactList;
 import com.x64technology.linex.screens.NewChat;
 import com.x64technology.linex.services.PreferenceManager;
+import com.x64technology.linex.services.SocketManager;
+
+import io.socket.client.Socket;
 
 public class MainActivity extends AppCompatActivity {
     ChatViewModel chatViewModel;
     ChatsAdapter chatsAdapter;
     ActivityMainBinding mainBinding;
+    Socket socket;
+    SocketManager socketManager;
+    PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,24 +51,36 @@ public class MainActivity extends AppCompatActivity {
 
         chatViewModel.getChats().observe(this, chats -> chatsAdapter.setChats(chats));
 
-        mainBinding.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.menu_contacts) {
-                    startActivity(new Intent(MainActivity.this, ContactList.class));
-                }
-                mainBinding.drawerLayout.close();
-                return false;
+        mainBinding.navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.menu_contacts) {
+                startActivity(new Intent(MainActivity.this, ContactList.class));
             }
+            mainBinding.drawerLayout.close();
+            return false;
         });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (new PreferenceManager(this).getUsername().equals("")) {
-//            startActivity(new Intent(this, Auth.class));
-//            finish();
+        preferenceManager = new PreferenceManager(this);
+        if (preferenceManager.getUsername().equals("")) {
+            startActivity(new Intent(this, Auth.class));
+            finish();
+        } else {
+            socketManager = new SocketManager(preferenceManager.getToken());
+            socket = socketManager.getSocket();
+            SocketManager.addSocketListeners(socket);
+            socket.connect();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (socket == null) return;
+        socket.disconnect();
+        SocketManager.removeSocketListeners(socket);
     }
 }
--- app/src/main/java/com/x64technology/linex/services/SocketManager.java
+++ app/src/main/java/com/x64technology/linex/services/SocketManager.java
@@ -1,4 +1,61 @@
 package com.x64technology.linex.services;
 
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 public class SocketManager {
+
+    IO.Options options;
+    private final Socket socket;
+
+    public SocketManager(String token) {
+        Map<String, String> test = new HashMap<>();
+        test.put("token", token);
+        options = new IO.Options();
+        options.auth = test;
+        {
+            try {
+                socket = IO.socket("http://192.168.43.30:3000", options);
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+
+  public static void addSocketListeners(Socket socket) {
+      socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+          @Override
+          public void call(Object... args) {
+              System.out.println("connected to socket");
+          }
+      });
+
+      socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+          @Override
+          public void call(Object... args) {
+              System.out.println("discounted");
+          }
+      });
+
+      socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+          @Override
+          public void call(Object... args) {
+              System.out.println("got connection error");
+          }
+      });
+  }
+
+    public static void removeSocketListeners(Socket socket) {
+      socket.off();
+    }
 }
